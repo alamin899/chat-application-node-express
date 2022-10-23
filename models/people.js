10d2a81ddd693c3890cd383defa46bd1dd6f9bbe
alaminAppnap <alamin@appnap.io>
2022-10-23 10:14:29 +0600

    [feat]:create people model

---
+++ models/people.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const peopleSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true
+    },
+    mobile:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    avater:{
+        type:String
+    },
+    role:{
+        type:String,
+        enum:["admin","user"],
+        default:"user"
+    },
+},
+{
+    timestamps:true  //schema method second parameter timestamps true means created_at and updated_at automatic set
+}
+);
+
+const People = mongoose.model("People",peopleSchema);
+
+module.exports = People;
