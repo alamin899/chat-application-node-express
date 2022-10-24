e8908d1bff513403edfd65c597394cd0f1d9f8b7
alaminhossen899 <alaminhossen899@gmail.com>
2022-10-24 21:04:04 +0600

    [refactor]:modified commit

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -7,18 +7,17 @@ const {getUsers,addUser,removeUser} = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 const avatarUpload = require("../middlewares/users/avatarUpload");
 const {addUserValidators,addUserValidationHandler} = require("../middlewares/users/userValidators");
-
 /** end internal imports */
 
 const router = express.Router();
 
-// users page
+/** users list route */
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
-// add user
+/** add user route */
 router.post("/",avatarUpload,addUserValidators,addUserValidationHandler,addUser);
 
-// remove user
+/** remove user router */
 router.delete("/:id", removeUser);
 
 module.exports = router;
